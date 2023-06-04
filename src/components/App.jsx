ff09abfbe2f11f323550371596bd62c90cc954b8
Andrii Doroshenko <doroshenkoandrey@ukr.net>
2023-06-04 22:13:05 +0300

    refactoring code with reducer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,27 +1,41 @@
-// import React, { Component } from 'react';
-import { useState } from 'react';
+import { useReducer } from 'react';
 import { Section } from '../components/SectionTitle/SectionTitle';
 import FeedbackOptions from '../components/FeedbackOptions/FeedbackOptions';
 import { Statistics } from '../components/Statistics/Statistics';
 import { Notification } from '../components/Notification/Notification';
 
+const initialState = {
+  good: 0,
+  neutral: 0,
+  bad: 0,
+};
+
+const feedbackReducer = (state, action) => {
+  switch (action.type) {
+    case 'good':
+      return { ...state, good: state.good + 1 };
+    case 'neutral':
+      return { ...state, neutral: state.neutral + 1 };
+    case 'bad':
+      return { ...state, bad: state.bad + 1 };
+
+    default:
+      return state;
+  }
+};
+
 export const App = () => {
-  const [feedbackState, setFeedbackState] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  });
+  const [feedbackState, dispatch] = useReducer(feedbackReducer, initialState);
 
   const onFeedbackCounterChange = event => {
     const name = event.target.name;
-    setFeedbackState(prevState => ({
-      ...prevState,
-      [name]: prevState[name] + 1,
-    }));
+    dispatch({ type: name });
   };
 
   const countTotalFeedback = () => {
-    return Object.values(feedbackState).reduce((acc, nextValue) => (acc += 1));
+    return Object.values(feedbackState).reduce(
+      (acc, nextValue) => (acc += nextValue)
+    );
   };
 
   const countPositiveFeedbackPercentage = () => {
