525a5b0b4d263a0de125a59278712794b64903c3
Andrii Doroshenko <doroshenkoandrey@ukr.net>
2023-06-04 16:22:09 +0300

    clear component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,69 +1,9 @@
 // import React, { Component } from 'react';
 import { useState } from 'react';
-
 import { Section } from '../components/SectionTitle/SectionTitle';
 import FeedbackOptions from '../components/FeedbackOptions/FeedbackOptions';
 import { Statistics } from '../components/Statistics/Statistics';
 import { Notification } from '../components/Notification/Notification';
-
-// export const App = () => {
-//   const [good, setGood] = useState(0);
-//   const [neutral, setNeutral] = useState(0);
-//   const [bad, setBad] = useState(0);
-
-//   const feedbackCounters = ['good', 'neutral', 'bad'];
-//   const feedbackCountersValues = [good, neutral, bad];
-
-//   const onfeedbackCountersChange = event => {
-//     const { name } = event.target;
-//     switch (name) {
-//       case 'good':
-//         setGood(prevState => prevState + 1);
-//         break;
-//       case 'neutral':
-//         setNeutral(prevState => prevState + 1);
-//         break;
-//       case 'bad':
-//         setBad(prevState => prevState + 1);
-//         break;
-
-//       default:
-//         break;
-//     }
-//   };
-
-//   const countTotalFeedback = () => {
-//     return good + neutral + bad;
-//   };
-
-//   const countPositiveFeedbackPercentage = () => {
-//     const total = countTotalFeedback();
-//     return total ? Math.round((good / total) * 100) : 0;
-//   };
-
-//   return (
-//     <>
-//       <Section title="Please leave feedback">
-//         <FeedbackOptions
-//           labels={feedbackCounters}
-//           onLeaveFeedback={onfeedbackCountersChange}
-//         />
-//       </Section>
-//       <Section title="Statistics">
-//         {countTotalFeedback() ? (
-//           <Statistics
-//             labels={feedbackCounters}
-//             values={feedbackCountersValues}
-//             countTotalFeedback={countTotalFeedback}
-//             percentage={countPositiveFeedbackPercentage}
-//           />
-//         ) : (
-//           <Notification message="There is no feedback"></Notification>
-//         )}
-//       </Section>
-//     </>
-//   );
-// };
 
 export const App = () => {
   const [feedbackState, setFeedbackState] = useState({
@@ -71,26 +11,6 @@ export const App = () => {
     neutral: 0,
     bad: 0,
   });
-
-  // const { good, neutral, bad } = feedbackState;
-
-  // const onfeedbackCountersChange = event => {
-  //   const { name } = event.target;
-  //   switch (name) {
-  //     case 'good':
-  //       setFeedbackState(prevState => prevState.good + 1);
-  //       break;
-  //     case 'neutral':
-  //       setFeedbackState(prevState => prevState.neutral + 1);
-  //       break;
-  //     case 'bad':
-  //       setFeedbackState(prevState => prevState.bad + 1);
-  //       break;
-
-  //     default:
-  //       break;
-  //   }
-  // };
 
   const onFeedbackCounterChange = event => {
     const name = event.target.name;
